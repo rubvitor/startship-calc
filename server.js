cb69e7e73324b10ffef0d71676ef42872146c445
Rubens Vitor <rubens.vitor@jdcinformatica.com.br>
2019-07-04 02:27:35 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+var bodyParser = require('body-parser');
 const app = express();
+ 
+app.use(express.static(`${__dirname}/src`));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(bodyParser.json());
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, 'src/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
