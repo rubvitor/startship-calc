ed2229b130eb686aae20ea7b963f329b26a00023
Rubens <rubens.vtr@gmail.com>
2019-07-04 11:41:22 -0300

    ajustes

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 4200);
